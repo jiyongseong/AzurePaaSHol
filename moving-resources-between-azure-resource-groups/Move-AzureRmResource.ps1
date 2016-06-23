2bf01ac7322a88e46d9fd9860f372f356987e505
jiyongseong <jiyongseong@hotmail.com>
2016-06-23 15:18:57 +0900

    adding move-azurermresource

--- moving-resources-between-azure-resource-groups/Move-AzureRmResource.ps1
+++ moving-resources-between-azure-resource-groups/Move-AzureRmResource.ps1
@@ -7,8 +7,6 @@ Select-AzureRmSubscription -SubscriptionName $subscriptionName #구독 선택
 $resourceGroupName = “source resource group”    #옮기려는 원본 리소스가 위치하고 있는 리소스 그룹
 $destResourceGroupName = “destination resource group” #옮길 대상 리소스 그룹 
 
- 
-
 Get-AzureRmResource | Where-Object {$_.ResourceGroupName -eq $resourceGroupName} | Select Name, ResourceType | ForEach-Object {
 $resource = Get-AzureRmResource –ResourceName $_.Name -ResourceGroupName $srcResourceGroupName
 Move-AzureRmResource -DestinationResourceGroupName $destResourceGroupName -ResourceId $resource.ResourceId
