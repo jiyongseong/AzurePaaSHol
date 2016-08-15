2025eff664949ae7c2d5b19e10278f6c5a01926b
jiyongseong <jiyongseong@hotmail.com>
2016-08-15 09:30:18 +0900

    codes

--- azure_sql/DMVs/plans/ad-hoc.sql
+++ azure_sql/DMVs/plans/ad-hoc.sql
@@ -1,4 +1,4 @@
-SELECT t.text AS sqlText, p.refcounts, p.usecount, p.size_in_bytes
+SELECT t.text AS sqlText, p.refcounts, p.usecounts, p.size_in_bytes
 FROM sys.dm_exec_cached_plans AS p CROSS APPLY sys.dm_exec_sql_text(p.plan_handle) AS t
-WHERE p.usecounts = 1 AND p.chaceobjtype = 'Compiled Plan' AND p.objtype = 'Adhoc';
+WHERE p.usecounts = 1 AND p.cacheobjtype = 'Compiled Plan' AND p.objtype = 'Adhoc';
 GO
--- azure_sql/DMVs/plans/query_stats_summary.sql
+++ azure_sql/DMVs/plans/query_stats_summary.sql
@@ -21,4 +21,4 @@ FROM sys.dm_exec_query_stats qs
 				CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) qt
 --				CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
 ORDER BY qs.total_worker_time/qs.execution_count DESC;
-GO
+GO 
