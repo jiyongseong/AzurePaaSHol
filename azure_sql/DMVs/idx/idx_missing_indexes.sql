c532966ff9800ca3681b0de2b1e73539c198567a
jiyongseong <jiyongseong@hotmail.com>
2016-09-18 07:36:50 +0900

    update missing idx.

--- azure_sql/DMVs/idx/idx_missing_indexes.sql
+++ azure_sql/DMVs/idx/idx_missing_indexes.sql
@@ -10,6 +10,6 @@ SELECT
   + ISNULL (' INCLUDE (' + mid.included_columns + ')', '') AS create_index_statement, 
   migs.*, mid.database_id, mid.[object_id]
 FROM sys.dm_db_missing_index_groups mig INNER JOIN sys.dm_db_missing_index_group_stats migs ON migs.group_handle = mig.index_group_handle
-								INNER JOIN sys.dm_db_missing_index_details mid ON mig.index_handle = mid.index_handle
+					INNER JOIN sys.dm_db_missing_index_details mid ON mig.index_handle = mid.index_handle
 WHERE migs.avg_total_user_cost * (migs.avg_user_impact / 100.0) * (migs.user_seeks + migs.user_scans) > 10
 ORDER BY migs.avg_total_user_cost * migs.avg_user_impact * (migs.user_seeks + migs.user_scans) DESC
