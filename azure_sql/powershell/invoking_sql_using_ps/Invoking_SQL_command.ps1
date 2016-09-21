9931ea556836a3eb13bce698459591b6bc343124
jiyongseong <jiyongseong@hotmail.com>
2016-09-22 08:10:01 +0900

    invoking sql cmd

---
+++ azure_sql/powershell/invoking_sql_using_ps/Invoking_SQL_command.ps1
@@ -0,0 +1,8 @@
+﻿
+$serverName = "your Azure SQL database Server name. eg,. yourserver.database.windows.net"
+$databaseName ="database name"
+$userName = "user name"
+$password = "password"
+$query = "SELECT * FROM sys.objects WHERE is_ms_shipped = 0"
+
+Invoke-Sqlcmd -ServerInstance $serverName -Database $databaseName -Username $userName -Password $password -Query $query | Format-Table
