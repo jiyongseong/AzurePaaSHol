2300f2eba127dd8469370f95a0a043288089a592
jiyongseong <jiyongseong@hotmail.com>
2016-11-05 12:00:14 +0900

    export redis cache database

---
+++ azure_automation/export_azure_redis_cache_db_periodically/export_redis_cache_database.ps1
@@ -0,0 +1,30 @@
+﻿$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+$resourceGroupName = "your resource group name"
+$redisServerName = "redis cache server name"
+$prefix = (Get-Date -Format yyyyMMddhhmm).ToString()
+$container = "SAS string"
+
+Export-AzureRmRedisCache -ResourceGroupName $resourceGroupName -Name $redisServerName -Prefix $prefix -Container $container
