d421ec4fa6f98c3c0bab3d91c3124e54e980c3c8
jiyongseong <jiyongseong@hotmail.com>
2016-11-06 10:18:41 +0900

    download azure redis cache module

---
+++ azure_automation/export_azure_redis_cache_db_periodically/download_azure_redis_cache_module.ps1
@@ -0,0 +1,17 @@
+﻿##http://blog.coretech.dk/jgs/azure-automation-script-for-downloading-and-preparing-azurerm-modules-for-azure-automation/
+
+$folder = "C:\azurePS"
+
+Find-Module -Name AzureRM.RedisCache | Save-Module -force -Path $folder
+
+$dirs = dir $folder -Directory
+
+$dirs | Foreach {
+    $source = $_.FullName
+    $destination = "$($_.FullName).zip"
+    
+    If(Test-path $destination) {Remove-item $destination}
+    
+    Add-Type -assembly "system.io.compression.filesystem"
+    [io.compression.zipfile]::CreateFromDirectory($Source, $destination,[System.IO.Compression.CompressionLevel]::Optimal,$true) 
+}
