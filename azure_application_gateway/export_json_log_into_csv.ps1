4dd416b5423c8aaa2ced701400b4b0319ae8099b
jiyongseong <jiyongseong@hotmail.com>
2017-03-02 14:43:07 +0900

    exporting json log into csv format

---
+++ azure_application_gateway/export_json_log_into_csv.ps1
@@ -0,0 +1,8 @@
+﻿$file = "csv file name and location"
+$records = Get-Content -Path "application gateway log and location"| ConvertFrom-Json | Select-Object -Expand records | select time, properties
+
+foreach ($record in $records)
+{
+    $record.properties | Add-Member -NotePropertyName "time" -NotePropertyValue  $record.time
+    $record.properties | Export-Csv -Path $file -NoTypeInformation -Append
+}
