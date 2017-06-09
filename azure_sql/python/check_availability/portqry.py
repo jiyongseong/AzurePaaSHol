cf8d30652f20387bfc0b38db9f41294212dc7daf
jiyongseong <jiyongseong@hotmail.com>
2017-06-09 14:37:50 +0900

    portqry Python version

---
+++ azure_sql/python/check_availability/portqry.py
@@ -0,0 +1,16 @@
+import socket
+from time import sleep, gmtime, strftime
+
+port = 1433
+host='<<your db server name>>.database.windows.net'
+sec = 10
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        print (str(strftime("%Y-%m-%d %H:%M:%S", gmtime())) + " '" + host + "' is listening on " + str(port) + ".")
+        s.close()
+    except socket.error as e:
+        print ("Error occured ({0}): {1}".format(e.errno, e.strerror))
+
+    sleep(sec)
