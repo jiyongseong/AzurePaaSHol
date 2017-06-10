a348d4924ce28de59cfec8d73b669fcbe91bcef8
jiyongseong <jiyongseong@hotmail.com>
2017-06-10 17:58:01 +0900

    check availability2

---
+++ azure_sql/powershell/check_availability2/script.ps1
@@ -0,0 +1,34 @@
+﻿$dbServerName = "<<your db server name>>.database.windows.net"
+$port = 1433
+$sleep = 10
+
+Clear-Host
+
+while ($true)
+{
+    $TCPClient = New-Object -TypeName System.Net.Sockets.TCPClient
+    try 
+    {
+        $TCPClient.Connect($dbServerName,$port)
+        if ($TCPClient.Connected -eq $true)
+        {
+            Write-Host ([string]::Format("{0}    '{1}' is listening on {2}.", (Get-Date -Format "yyyy-MM-dd HH:mm:ss").ToString(), $dbServerName, $port))
+        }
+        else
+        {
+            Write-Host ([string]::Format("{0}    Connected. but, '{1}' is not listening on {2}.", (Get-Date -Format "yyyy-MM-dd HH:mm:ss").ToString(), $dbServerName, $port))
+        }
+    }
+    catch
+    {
+        Write-Host ([string]::Format("{0}    Error occured ({1}): {2}", (Get-Date -Format "yyyy-MM-dd HH:mm:ss").ToString(), $_.Exception.Message, $_.Exception.ItemName))
+    }
+    finally
+    {
+        $TCPClient.Close()
+        $TCPClient.Dispose()
+    }
+
+    Start-Sleep -Seconds $sleep
+}
+
