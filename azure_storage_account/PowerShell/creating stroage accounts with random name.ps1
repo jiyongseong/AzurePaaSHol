8f3bd84ed87783ed80f31771c04f7aac8c5376df
Ji Yong Seong <jyseong@microsoft.com>
2019-04-21 15:47:14 +0900

    #random_name #sotrage_account

---
+++ azure_storage_account/PowerShell/creating stroage accounts with random name.ps1
@@ -0,0 +1,20 @@
+﻿#https://blogs.technet.microsoft.com/heyscriptingguy/2015/11/05/generate-random-letters-with-powershell/
+
+$resourceGroup = "<<resource group name>>"
+$location = "<<location>>"
+
+Remove-AzureRmResourceGroup -Name $resourceGroup -Force
+New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+
+$seq = 0
+$count = 10
+
+while($seq -lt $count)
+{
+    $storageAccountNamePrefix = -join ((97..122) | Get-Random -Count 8 | % {[char]$_})
+    $storageAccountNamePostfix = "0" * (3- $seq.ToString().Length) + $seq.ToString()
+
+    Write-Host $storageAccountNamePrefix$storageAccountNamePostfix
+    New-AzureRmStorageAccount -ResourceGroupName $resourceGroup -Name $storageAccountNamePrefix$storageAccountNamePostfix -SkuName Standard_LRS -Location $location -Kind StorageV2 
+    $seq += 1
+}
