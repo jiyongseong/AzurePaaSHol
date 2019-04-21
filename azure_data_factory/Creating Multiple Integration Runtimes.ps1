edb0a7d568552afd0a71796461f48aad4facffdc
Ji Yong Seong <jyseong@microsoft.com>
2019-04-21 12:50:15 +0900

    Set-AzDataFactoryV2IntegrationRuntime

---
+++ azure_data_factory/Creating Multiple Integration Runtimes.ps1
@@ -0,0 +1,13 @@
+﻿
+Select-AzSubscription -Subscription "<<subscription name>>"
+
+$rg = "<<resource group name>>"
+$dataFactoryName = "<<data factory name>>"
+$location = "EAST US"
+
+##Create Integration Runtime
+for ($seq = 0; $seq -le 19;$seq++)
+{
+    $irName = "IR-EASTUS-"+ "0"*(2-$seq.ToString().Length) + $seq.ToString()
+    Set-AzDataFactoryV2IntegrationRuntime -ResourceGroupName $rg -DataFactoryName $dataFactoryName -Name $irName -Type Managed -Location $location
+}
